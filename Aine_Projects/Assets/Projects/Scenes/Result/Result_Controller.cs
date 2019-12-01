3bb1d1dfde8756f2b35dcace193bd045e07201d0
Alseret <bakemono_0601@yahoo.co.jp>
2019-12-02 01:27:54 +0900

    [Aine_Projects] Result修正

---
+++ Aine_Projects/Assets/Projects/Scenes/Result/Result_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Result_Controller : MonoBehaviour
+{
+	[SerializeField] private Image m_capture;
+	[SerializeField] private ScreenShot m_scrShot;
+	[SerializeField] private Transform m_target;
+	[SerializeField] private GameObject[] m_effects;
+	[SerializeField] private TextMeshProUGUI m_text;
+	[SerializeField] private int m_cnt;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		m_scrShot = GameObject.Find("Cam").GetComponent<ScreenShot>();
+		m_cnt = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			m_capture.sprite = m_scrShot.m_sprite;
+			transform.GetChild(0).GetComponent<Image>().enabled = true;
+			transform.GetChild(0).GetChild(0).GetComponent<Image>().enabled = true;
+			transform.GetChild(1).GetComponent<TextMeshProUGUI>().enabled = true;
+		}
+		if(Input.GetKey(KeyCode.P))
+		{
+			Instantiate(m_effects[Random.Range(0, m_effects.Length)],
+						new Vector2(Random.Range(m_target.position.x - 170f, m_target.position.x + 170f),
+						m_target.position.y), Quaternion.identity, m_target);
+			m_cnt++;
+			m_text.text = "♪ : " + m_cnt;
+		}
+	}
+}
---
+++ Aine_Projects/Assets/Projects/Scenes/Result/ScreenShot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenShot : MonoBehaviour
+{
+	[SerializeField] private Texture2D m_tex;
+	[SerializeField] public Sprite m_sprite;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//m_target = GameObject.Find("Result_Capture").GetComponent<Image>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.B))
+			StartCoroutine(imageShot());
+	}
+
+	private IEnumerator imageShot()
+	{
+		yield return new WaitForEndOfFrame();
+		string name = DateTime.Now.ToString("HH:mm:ss") + ".jpg";
+		m_tex = new Texture2D((int)Screen.width, (int)Screen.height, TextureFormat.RGB24, false);
+		m_tex.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height), 0, 0);
+		m_tex.Apply();
+		m_sprite = Sprite.Create(m_tex, new Rect(0f, 0f, m_tex.width, m_tex.height), new Vector2(.5f, .5f), 1f);
+
+		yield return null;
+	}
+}
